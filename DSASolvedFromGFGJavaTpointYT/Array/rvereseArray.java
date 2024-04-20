b0a0fc939bcfa782cd5affa2583ea7085daff434
Rosalin <rosalinkandapan0398@gmail.com>
2024-04-20 22:07:37 +0530

    Array

--- DSASolvedFromGFGJavaTpointYT/Array/rvereseArray.java
+++ DSASolvedFromGFGJavaTpointYT/Array/rvereseArray.java
@@ -22,7 +22,7 @@ public class rvereseArray {
 	}
 
 	public static void main(String[] args) {
-		int arr[] = { 1, 2, 3, 4, 5, 6 };
+		int arr[] = { 1, 2, 3, 4, 5, 6 ,8};
 		printArray(arr, 6);
 		reverse(arr, 0, 5);
 		System.out.print("Reversed array is \n");
